fcfd2f3c184ef22ba0cd221b6168eb5b0402665a
FogDong <dongtianxin.tx@alibaba-inc.com>
2022-07-11 17:05:30 +0800

    Feat: add workflow metrics
    Signed-off-by: FogDong <dongtianxin.tx@alibaba-inc.com>

---
+++ pkg/monitor/metrics/workflow.go
@@ -0,0 +1,96 @@
+/*
+Copyright 2022 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
+)
+
+var histogramBuckets = []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
+	1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 40, 50, 60}
+
+var (
+	//	WorkflowRunReconcileTimeHistogram report the reconciling time cost of workflow run controller with state transition recorded
+	WorkflowRunReconcileTimeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:        "workflowrun_reconcile_time_seconds",
+		Help:        "workflow run reconcile duration distributions.",
+		Buckets:     histogramBuckets,
+		ConstLabels: prometheus.Labels{},
+	}, []string{"begin_phase", "end_phase"})
+
+	// GenerateTaskRunnerDurationHistogram report the generate task runners execution duration.
+	GenerateTaskRunnersDurationHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:        "generate_task_runners_time_seconds",
+		Help:        "generate task runners duration distributions.",
+		Buckets:     histogramBuckets,
+		ConstLabels: prometheus.Labels{},
+	}, []string{"controller"})
+
+	// WorkflowRunPhaseCounter report the number of workflow run phase
+	WorkflowRunPhaseCounter = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "workflow_phase_number",
+		Help: "workflow run phase number",
+	}, []string{"phase"})
+
+	// WorkflowRunFinishedTimeHistogram report the time for finished workflow run
+	WorkflowRunFinishedTimeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:        "workflow_finished_time_seconds",
+		Help:        "workflow finished time distributions.",
+		Buckets:     histogramBuckets,
+		ConstLabels: prometheus.Labels{},
+	}, []string{"phase"})
+
+	// WorkflowRunInitializedCounter report the workflow run initialize execute number.
+	WorkflowRunInitializedCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "workflow_initialized_num",
+		Help: "workflow initialize times",
+	}, []string{})
+
+	// WorkflowStepPhaseGauge report the number of workflow step state
+	WorkflowStepPhaseGauge = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "workflow_step_phase_number",
+		Help: "workflow step phase number",
+	}, []string{"step_type", "phase"})
+
+	// WorkflowStepDurationHistogram report the step execution duration.
+	WorkflowStepDurationHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:        "step_duration_ms",
+		Help:        "step latency distributions.",
+		Buckets:     histogramBuckets,
+		ConstLabels: prometheus.Labels{},
+	}, []string{"controller", "step_type"})
+)
+
+var collectorGroup = []prometheus.Collector{
+	GenerateTaskRunnersDurationHistogram,
+	WorkflowStepDurationHistogram,
+	WorkflowRunReconcileTimeHistogram,
+	WorkflowRunFinishedTimeHistogram,
+	WorkflowRunInitializedCounter,
+	WorkflowRunPhaseCounter,
+	WorkflowStepPhaseGauge,
+}
+
+func init() {
+	for _, collector := range collectorGroup {
+		if err := metrics.Registry.Register(collector); err != nil {
+			klog.Error(err)
+		}
+	}
+}
---
+++ pkg/monitor/watcher/workflow.go
@@ -0,0 +1,125 @@
+/*
+Copyright 2022 The KubeVela Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package watcher
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"k8s.io/client-go/tools/cache"
+	ctrlcache "sigs.k8s.io/controller-runtime/pkg/cache"
+
+	"github.com/kubevela/workflow/api/v1alpha1"
+	"github.com/kubevela/workflow/pkg/monitor/metrics"
+)
+
+type workflowRunMetricsWatcher struct {
+	mu               sync.Mutex
+	phaseCounter     map[string]int
+	stepPhaseCounter map[string]int
+	phaseDirty       map[string]struct{}
+	stepPhaseDirty   map[string]struct{}
+	informer         ctrlcache.Informer
+	stopCh           chan struct{}
+}
+
+func (watcher *workflowRunMetricsWatcher) getRun(obj interface{}) *v1alpha1.WorkflowRun {
+	wr := &v1alpha1.WorkflowRun{}
+	bs, _ := json.Marshal(obj)
+	_ = json.Unmarshal(bs, wr)
+	return wr
+}
+
+func (watcher *workflowRunMetricsWatcher) getPhase(phase string) string {
+	if phase == "" {
+		return "-"
+	}
+	return phase
+}
+
+func (watcher *workflowRunMetricsWatcher) inc(wr *v1alpha1.WorkflowRun, delta int) {
+	watcher.mu.Lock()
+	defer watcher.mu.Unlock()
+	phase := watcher.getPhase(string(wr.Status.Phase))
+	watcher.phaseCounter[phase] += delta
+	watcher.phaseDirty[phase] = struct{}{}
+	for _, step := range wr.Status.Steps {
+		stepPhase := watcher.getPhase(string(step.Phase))
+		key := fmt.Sprintf("%s/%s#%s", step.Type, stepPhase, step.Reason)
+		watcher.stepPhaseCounter[key] += delta
+		watcher.stepPhaseDirty[key] = struct{}{}
+	}
+}
+
+func (watcher *workflowRunMetricsWatcher) report() {
+	watcher.mu.Lock()
+	defer watcher.mu.Unlock()
+	for phase := range watcher.phaseDirty {
+		metrics.WorkflowRunPhaseCounter.WithLabelValues(phase).Set(float64(watcher.phaseCounter[phase]))
+	}
+	for stepPhase := range watcher.stepPhaseDirty {
+		metrics.WorkflowStepPhaseGauge.WithLabelValues(strings.Split(stepPhase, "/")...).Set(float64(watcher.stepPhaseCounter[stepPhase]))
+	}
+	watcher.phaseDirty = map[string]struct{}{}
+	watcher.stepPhaseDirty = map[string]struct{}{}
+}
+
+func (watcher *workflowRunMetricsWatcher) run() {
+	go func() {
+		for {
+			select {
+			case <-watcher.stopCh:
+				return
+			default:
+				time.Sleep(time.Second)
+				watcher.report()
+			}
+		}
+	}()
+}
+
+// StartWorkflowRunMetricsWatcher start metrics watcher for reporting workflow run stats
+func StartWorkflowRunMetricsWatcher(informer ctrlcache.Informer) {
+	watcher := &workflowRunMetricsWatcher{
+		phaseCounter:     map[string]int{},
+		stepPhaseCounter: map[string]int{},
+		phaseDirty:       map[string]struct{}{},
+		stepPhaseDirty:   map[string]struct{}{},
+		informer:         informer,
+		stopCh:           make(chan struct{}, 1),
+	}
+	watcher.informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			wr := watcher.getRun(obj)
+			watcher.inc(wr, 1)
+		},
+		UpdateFunc: func(oldObj, obj interface{}) {
+			oldWr := watcher.getRun(oldObj)
+			wr := watcher.getRun(obj)
+			watcher.inc(oldWr, -1)
+			watcher.inc(wr, 1)
+		},
+		DeleteFunc: func(obj interface{}) {
+			wr := watcher.getRun(obj)
+			watcher.inc(wr, -1)
+		},
+	})
+	watcher.run()
+}
