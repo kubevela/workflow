2285ddcbe5c7ac3f34f8a8ca7ca2b5fffffa5426
FogDong <dongtianxin.tx@alibaba-inc.com>
2022-08-08 18:15:26 +0800

    Fix: do not import kubevela package in workflow
    Signed-off-by: FogDong <dongtianxin.tx@alibaba-inc.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -34,8 +34,6 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	"github.com/oam-dev/kubevela/apis/core.oam.dev/v1beta1"
-
 	"github.com/kubevela/workflow/api/v1alpha1"
 	"github.com/kubevela/workflow/pkg/cue/packages"
 	//+kubebuilder:scaffold:imports
@@ -81,8 +79,6 @@ var _ = BeforeSuite(func() {
 	err = v1alpha1.AddToScheme(testScheme)
 	Expect(err).NotTo(HaveOccurred())
 
-	err = v1beta1.SchemeBuilder.AddToScheme(testScheme)
-	Expect(err).NotTo(HaveOccurred())
 	//+kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: testScheme})
