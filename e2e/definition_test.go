ac7e7f643b0812d2077395c221fbf83e86672e72
FogDong <dongtianxin.tx@alibaba-inc.com>
2022-10-19 11:43:25 +0800

    Feat: add more e2e tests for built-in providers
    Signed-off-by: FogDong <dongtianxin.tx@alibaba-inc.com>

--- e2e/definition_test.go
+++ e2e/definition_test.go
@@ -1,9 +1,12 @@
 /*
 Copyright 2022 The KubeVela Authors.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
 	http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,14 +33,14 @@ import (
 	"github.com/oam-dev/kubevela/pkg/oam/util"
 )
 
-var _ = Describe("Test the workflow run with the config steps", func() {
+var _ = Describe("Test the workflow run with the built-in definitions", func() {
 	ctx := context.Background()
 
 	var namespace string
 	var ns corev1.Namespace
 
 	BeforeEach(func() {
-		namespace = "config-e2e-test"
+		namespace = "wr-e2e-test"
 		ns = corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}
 
 		Eventually(func() error {
@@ -45,17 +48,26 @@ var _ = Describe("Test the workflow run with the config steps", func() {
 		}, time.Second*3, time.Microsecond*300).Should(SatisfyAny(BeNil(), &util.AlreadyExistMatcher{}))
 	})
 
-	It("Test the workflow", func() {
-		content, err := os.ReadFile("./test-data/config-workflow-run.yaml")
-		Expect(err).Should(BeNil())
-		var workflowRun v1alpha1.WorkflowRun
-		Expect(yaml.Unmarshal(content, &workflowRun)).Should(BeNil())
-		workflowRun.Namespace = namespace
-		Expect(k8sClient.Create(context.TODO(), &workflowRun)).Should(BeNil())
+	It("Test the workflow with config definition", func() {
+		wr := applyWorkflowRunFromYAML(ctx, "./test-data/config-workflow-run.yaml", namespace)
 		Eventually(
 			func() v1alpha1.WorkflowRunPhase {
 				var getWorkflow v1alpha1.WorkflowRun
-				if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: workflowRun.Name}, &getWorkflow); err != nil {
+				if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: wr.Name}, &getWorkflow); err != nil {
+					klog.Errorf("fail to query the app %s", err.Error())
+				}
+				klog.Infof("the workflow run status is %s (%+v)", getWorkflow.Status.Phase, getWorkflow.Status.Steps)
+				return getWorkflow.Status.Phase
+			},
+			time.Second*30, time.Second*2).Should(Equal(v1alpha1.WorkflowStateSucceeded))
+	})
+
+	It("Test the workflow with message definition", func() {
+		wr := applyWorkflowRunFromYAML(ctx, "./test-data/message-workflow-run.yaml", namespace)
+		Eventually(
+			func() v1alpha1.WorkflowRunPhase {
+				var getWorkflow v1alpha1.WorkflowRun
+				if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: wr.Name}, &getWorkflow); err != nil {
 					klog.Errorf("fail to query the app %s", err.Error())
 				}
 				klog.Infof("the workflow run status is %s (%+v)", getWorkflow.Status.Phase, getWorkflow.Status.Steps)
@@ -69,3 +81,13 @@ var _ = Describe("Test the workflow run with the config steps", func() {
 		k8sClient.DeleteAllOf(ctx, &v1alpha1.WorkflowRun{}, client.InNamespace(namespace))
 	})
 })
+
+func applyWorkflowRunFromYAML(ctx context.Context, path, ns string) v1alpha1.WorkflowRun {
+	content, err := os.ReadFile(path)
+	Expect(err).Should(BeNil())
+	var workflowRun v1alpha1.WorkflowRun
+	Expect(yaml.Unmarshal(content, &workflowRun)).Should(BeNil())
+	workflowRun.Namespace = ns
+	Expect(k8sClient.Create(ctx, &workflowRun)).Should(BeNil())
+	return workflowRun
+}
